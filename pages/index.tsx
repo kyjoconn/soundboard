fbe06db61287ffae1c810aa7790e7a6e098da9ac
Kyle OConnor <kyjoconn@gmail.com>
2023-01-08 16:46:52 -0800

    css

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,30 +17,24 @@ export default function Home() {
       <main className={styles.main}>
         <h3 className="m-4 ml-0">New Boards</h3>
         <div className={styles.newBoards}>
-          <div className={styles.newBoards}>
             <div className={`border ${styles.boardCard}`}>
               <div className={styles.boardImg} style={{backgroundImage: "url(https://cdn.openai.com/labs/images/A%20photo%20of%20a%20white%20fur%20monster%20standing%20in%20a%20purple%20room.webp?v=1)"}} />
               <div className="p-4">
                 Monsters Inc.
               </div>
             </div>
-          </div>
-          <div className={styles.newBoards}>
             <div className={`border ${styles.boardCard}`}>
               <div className={styles.boardImg} style={{backgroundImage: "url(https://static01.nyt.com/images/2021/04/04/arts/04godzilla-kong1/04godzilla-kong1-videoSixteenByNineJumbo1600.jpg?year=2021&h=900&w=1600&s=29019e1d9dcda2d49a3d1becdc9b372eca17cc7f42fc20d6d0047bffe344c11b&k=ZQJBKqZ0VN&tw=1)"}} />
               <div className="p-4">
                 Godzilla
               </div>
             </div>
-          </div>
-          <div className={styles.newBoards}>
             <div className={`border ${styles.boardCard}`}>
               <div className={styles.boardImg} style={{backgroundImage: "url(https://wwwimage-us.pplusstatic.com/thumbnails/photos/w1920-q80/marquee/1040727/rockp_sp_hero_landscape.jpg)"}} />
               <div className="p-4">
                 Rocket Power
               </div>
             </div>
-          </div>
         </div>
         <div className={styles.newBoardCt}>
           <button type="button" className="border font-medium rounded-lg text-sm px-5 py-2.5 text-center">Create New Board</button>
